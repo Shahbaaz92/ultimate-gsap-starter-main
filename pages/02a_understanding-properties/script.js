253b3cf1941f8d37f1146f75d8e196da9cab73ca
Shahbaaz Athhar <59239856+Shahbaaz92@users.noreply.github.com>
2025-08-01 12:17:38 +0530

    Update script.js

--- pages/02a_understanding-properties/script.js
+++ pages/02a_understanding-properties/script.js
@@ -3,10 +3,12 @@ gsap.to(".box", {
   rotation: 360,
   background: "#ff6f61",
   borderRadius: "50%",
-  duration: 5,
+  duration: 2,
   delay: 1,
   scale: 0.95,
   x: 200,
   y: -100,
   ease: "power1.inOut",
+  repeat: -1,
+  yoyo: true,
 });
