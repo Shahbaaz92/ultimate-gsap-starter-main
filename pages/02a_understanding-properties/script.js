b3912686ace3e478aead37708356908938457c82
Shahbaaz Athhar <59239856+Shahbaaz92@users.noreply.github.com>
2025-08-01 09:32:51 +0530

    Update

---
+++ pages/02a_understanding-properties/script.js
@@ -0,0 +1,11 @@
+import gsap from "gsap";
+gsap.to(".box", {
+  opacity: 1,
+  rotation: 360,
+  background: "#ff6f61",
+  borderRadius: "50%",
+  duration: 5,
+  scale: 1.05,
+  x: 200,
+  y: -100,
+});
