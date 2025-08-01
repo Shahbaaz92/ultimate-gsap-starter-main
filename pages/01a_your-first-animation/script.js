c42591f273ae39660fbe5ca95ce26f0727f241ea
Shahbaaz Athhar <59239856+Shahbaaz92@users.noreply.github.com>
2025-08-01 08:22:41 +0530

    Update script.js

--- pages/01a_your-first-animation/script.js
+++ pages/01a_your-first-animation/script.js
@@ -4,4 +4,12 @@ gsap.to(".card", {
   opacity: 1,
   scale: 1,
   duration: 3,
+  onComplete: () => {
+    gsap.to(".card", {
+      y: -20,
+      duration: 0.5,
+      yoyo: true,
+      repeat: -1,
+    });
+  },
 });
