f2dd99a15e2ac686150c18093b3f51d33d758ba9
Shahbaaz Athhar <59239856+Shahbaaz92@users.noreply.github.com>
2025-08-04 17:28:43 +0530

    Update script.js

---
+++ pages/11a_learn-from/script.js
@@ -0,0 +1,4 @@
+//TODO - Learn GsapFrom
+// The gsap.from() method is used to animate elements from a new state to their current state.
+
+// The gsap.from() method is similar to the gsap.to() method, but the difference is that the gsap.from() method animates elements from a new state to their current state, while the gsap.to() method animates elements from their current state to a new state.
