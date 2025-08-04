ae9bcfff268a874cb4e799765bb168e21180f954
Shahbaaz Athhar <59239856+Shahbaaz92@users.noreply.github.com>
2025-08-04 17:50:21 +0530

    Update script.js

--- pages/11a_learn-from/script.js
+++ pages/11a_learn-from/script.js
@@ -5,27 +5,44 @@
 
 import gsap from "gsap";
 
-gsap.from(".card-1", {
+// gsap.from(".card-1", {
+//   y: 60,
+//   opacity: 0,
+//   scale: 0.95,
+//   duration: 1,
+//   delay: 0.3,
+//   ease: "power4.out",
+// });
+// gsap.from(".card-2", {
+//   y: 60,
+//   opacity: 0,
+//   scale: 0.95,
+//   duration: 1,
+//   delay: 0.6,
+//   ease: "power4.out",
+// });
+// gsap.from(".card-3", {
+//   y: 60,
+//   opacity: 0,
+//   scale: 0.95,
+//   duration: 1,
+//   delay: 0.9,
+//   ease: "power4.out",
+// });
+
+const repeat = document.querySelector(".repeat");
+
+//you can name your animation and then restart it whenever you want.
+const animation = gsap.from(".card", {
   y: 60,
   opacity: 0,
   scale: 0.95,
   duration: 1,
   delay: 0.3,
   ease: "power4.out",
+  stagger: 0.2, //Stagger property
 });
-gsap.from(".card-2", {
-  y: 60,
-  opacity: 0,
-  scale: 0.95,
-  duration: 1,
-  delay: 0.6,
-  ease: "power4.out",
-});
-gsap.from(".card-3", {
-  y: 60,
-  opacity: 0,
-  scale: 0.95,
-  duration: 1,
-  delay: 0.9,
-  ease: "power4.out",
+
+repeat.addEventListener("click", () => {
+  animation.restart();
 });
