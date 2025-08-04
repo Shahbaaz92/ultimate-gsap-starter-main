43fcca483bf641acec632ba5a0b01bb74847fe8f
Shahbaaz Athhar <59239856+Shahbaaz92@users.noreply.github.com>
2025-08-04 07:10:49 +0530

    Update script.js

---
+++ pages/06a_bounce-easing/script.js
@@ -0,0 +1,16 @@
+//TODO - Bounce Easing
+
+// Bounce easing simulates the behavior of a physical object falling and bouncing before settling.
+// It’s not about subtlety, it’s about fun, impact, and attention.
+
+//* In GSAP, you get three variations of Bouncing:
+
+// ease: "bounce.in"     // bounces as it enters
+// ease: "bounce.out"    // bounces as it finishes
+// ease: "bounce.inOut"  // bounces on both ends
+
+//? When to use?
+
+// Use bounce easing when you want an animation to feel playful or energetic, or Mimic physics (falling, landing, dropping) that grabs the user’s attention.
+
+// It’s ideal for toasts or alerts dropping in, cards landing into place, floating UI elements snapping into view, or button entrances on onboarding or gamified flows.
